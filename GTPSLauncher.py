12361fdc99fc8ca6a2a9d35b2442fafd3358e5c3
UazeH <89394300+UazeH@users.noreply.github.com>
2022-03-09 08:42:37 +0700

    Add files via upload

---
+++ GTPSLauncher.py
@@ -0,0 +1,54 @@
+import time
+import os
+
+def list():
+    os.system("title Menu List")
+    os.system("color 0c")
+    os.system('cls')
+    print("""[+] Menu :
+    
+[1] Turn ON GTPSLauncher
+[2] Turn OFF GTPSLauncher
+""")
+
+    pilih = input("[>] Select Menu: ")
+
+    if pilih == "1":on()
+    elif pilih == '2':off()
+    else:
+        os.system("cls")
+        print("Menu Not Available!")
+        print(" ")
+        y = input("Back To List Menu? (y/n): ")
+    if y == "y":
+        list()
+
+def on():
+    f = open("C:\\Windows\\System32\\drivers\\etc\\hosts", "w")
+    f.write(f"youripserver growtopia1.com\nyouripserver growtopia2.com\n")
+    f.close()
+
+    print("Please wait......")
+    time.sleep(5)
+
+    os.system("cls")
+    print("GTPSLauncher has been turned ON")
+    y = input("Back To List Menu? (y/n): ")
+    if y == "y":
+        list()
+
+def off():
+    f = open("C:\\Windows\\System32\\drivers\\etc\\hosts", "w")
+    f.write(f"")
+    f.close()
+
+    print("Please wait......")
+    time.sleep(5)
+
+    os.system("cls")
+    print("GTPSLauncher has been turned OFF")
+    y = input("Back To List Menu? (y/n): ")
+    if y == "y":
+        list()
+
+list()
